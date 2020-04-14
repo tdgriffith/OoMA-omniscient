a508cb51777f5d7c68bf3a4b847efa9a5952d296
tdgriffith <griffith@iastate.edu>
2020-04-14 14:25:44 -0500

    add cookiecutter structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Operational modal analysis on the DEAP dataset. Searching for modes in cognition with traditional structural analysis.',
+    author='T. Griffith',
+    license='MIT',
+)
